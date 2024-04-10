e7eb420733dcf7ea6ae81eae75baf19030b81e1d
erick <1922151955@qq.com>
2024-04-10 17:31:30 +0800

    code: update pinia

--- code/vue-example/src/pages/pinia/index.vue
+++ code/vue-example/src/pages/pinia/index.vue
@@ -1,18 +1,24 @@
 <template>
     <div>userStore: {{ userStore }}</div>
     <div>gender: {{ userStore.genderText }}</div>
+    <div>nick: {{ nick }}</div>
+    <div>hobby: {{ hobby }}</div>
+    <div>token: {{ token }}</div>
 </template>
 
 <script setup>
 import { onMounted } from 'vue'
+import { storeToRefs } from 'pinia'
 import { useUserStore } from '@/store/User-composition.js'
 // userStore是`reactive`的响应式数据
 const userStore = useUserStore()
 
 // 访问state
-console.log("userStore", userStore)
+console.log("userStore", userStore)     // Proxy(Object) {$id: 'user', $onAction: ƒ, $patch: ƒ, $reset: ƒ, $subscribe: ƒ, …}
 console.log("userStore.nick", userStore.nick)	// tom
 console.log("userStore.$state.nick", userStore.$state.nick)	// tom
+// 单个访问比较繁琐，可以使用`pinia`提供的`storeToRefs`方法，不同于`toRefs`的是，`storeToRefs`只会将`state`数据进行转换。
+const { nick, hobby, token } = storeToRefs(userStore)
 
 // 修改state
 userStore.nick = 'jerry'
