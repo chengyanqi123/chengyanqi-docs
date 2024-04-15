7168dee36719075e38b3554419610c027d211b05
erick <1922151955@qq.com>
2024-04-15 15:52:26 +0800

    docs: 新增微信小程序文档

--- docs/.vitepress/config/nav.js
+++ docs/.vitepress/config/nav.js
@@ -4,25 +4,25 @@ export default [
         items: [
             {
                 text: 'HTML',
-                link: '/语言基础/HTML'
+                link: '/语言基础/HTML',
             },
             {
                 text: 'CSS',
-                link: '/语言基础/CSS'
+                link: '/语言基础/CSS',
             },
             {
                 text: 'JavaScript',
-                link: '/语言基础/JavaScript'
+                link: '/语言基础/JavaScript',
             },
             {
                 text: 'Nodejs',
-                link: '/语言基础/Nodejs'
+                link: '/语言基础/Nodejs',
             },
             {
                 text: 'TypeScript',
-                link: '/语言基础/TypeScript'
-            }
-        ]
+                link: '/语言基础/TypeScript',
+            },
+        ],
     },
     {
         text: '框架',
@@ -43,9 +43,24 @@ export default [
             },
             {
                 text: 'Electron',
-                link: '/框架/Electron'
-            }
-        ]
+                link: '/框架/Electron',
+            },
+        ],
+    },
+    {
+        text: '微信小程序',
+        items: [
+            {
+                text: '基础',
+                // items:[]
+                link: '/微信小程序/小程序基础/01-小程序基础',
+            },
+            {
+                text: '项目实战',
+                // items:[]
+                link: '/微信小程序/慕尚花坊项目/01-项目介绍',
+            },
+        ],
     },
     {
         text: '工程化',
@@ -55,60 +70,60 @@ export default [
                 items: [
                     { text: 'webpack', link: '/工程化/打包工具/webpack.md' },
                     { text: 'rollup', link: '/工程化/打包工具/rollup.md' },
-                    { text: 'vite', link: '/工程化/打包工具/vite.md' }
-                ]
+                    { text: 'vite', link: '/工程化/打包工具/vite.md' },
+                ],
             },
             {
                 text: '包管理器',
                 items: [
                     { text: 'npm', link: '/工程化/包管理器/npm.md' },
                     { text: 'pnpm', link: '/工程化/包管理器/pnpm.md' },
-                    { text: 'yarn', link: '/工程化/包管理器/yarn.md' }
-                ]
-            }
-        ]
+                    { text: 'yarn', link: '/工程化/包管理器/yarn.md' },
+                ],
+            },
+        ],
     },
     {
         text: '面试题',
         items: [
             {
                 text: 'CSS面试题',
-                link: '/面试题/CSS面试题'
+                link: '/面试题/CSS面试题',
             },
             {
                 text: 'HTML面试题',
-                link: '/面试题/HTML面试题'
+                link: '/面试题/HTML面试题',
             },
             {
                 text: 'Vue面试题',
-                link: '/面试题/Vue面试题'
+                link: '/面试题/Vue面试题',
             },
             {
                 text: 'JavaScript面试题',
-                link: '/面试题/JavaScript面试题'
+                link: '/面试题/JavaScript面试题',
             },
             {
                 text: 'webpack面试题',
-                link: '/面试题/webpack面试题'
-            }
-        ]
+                link: '/面试题/webpack面试题',
+            },
+        ],
     },
     {
         text: '进阶后端',
         items: [
             {
                 text: 'Nginx',
-                link: '/进阶后端/Nginx'
+                link: '/进阶后端/Nginx',
             },
             {
                 text: 'Mysql',
-                link: '/进阶后端/Mysql'
+                link: '/进阶后端/Mysql',
             },
             {
                 text: 'Redis',
-                link: '/进阶后端/Redis'
-            }
-        ]
+                link: '/进阶后端/Redis',
+            },
+        ],
     },
     {
         text: '常用库及工具',
@@ -119,47 +134,48 @@ export default [
                 items: [
                     {
                         text: 'dayjs',
-                        link: 'https://github.com/iamkun/dayjs/blob/dev/docs/zh-cn/README.zh-CN.md'
+                        link: 'https://github.com/iamkun/dayjs/blob/dev/docs/zh-cn/README.zh-CN.md',
                     },
-                ]
+                ],
             },
             {
                 text: '前端',
                 items: [
                     {
                         text: 'vue-router',
-                        link: '/常用库及工具/vue-router'
+                        link: '/常用库及工具/vue-router',
                     },
                     {
                         text: 'pinia',
-                        link: '/常用库及工具/pinia'
+                        link: '/常用库及工具/pinia',
                     },
                     {
                         text: 'element-plus',
-                        link: 'https://element-plus.org/zh-CN/guide/installation.html'
+                        link: 'https://element-plus.org/zh-CN/guide/installation.html',
                     },
-                ]
+                ],
             },
             {
                 text: 'Nodejs',
-                items: []
+                items: [],
             },
             {
                 text: 'cli',
-                items: []
+                items: [],
             },
-        ]
+        ],
     },
     {
         text: '常见问题',
-        link: '/常见问题'
+        link: '/常见问题',
     },
     {
-        text: '我的开源', items: [
+        text: '我的开源',
+        items: [
             {
                 text: 'm3u8-conver',
-                link: 'https://github.com/Chengyanqi123/m3u8-conver'
-            }
-        ]
+                link: 'https://github.com/Chengyanqi123/m3u8-conver',
+            },
+        ],
     },
 ]
